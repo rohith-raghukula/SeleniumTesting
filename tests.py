d5b20f093aa4d6cb3ec5b9fcc703e390bfe0b137
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-02-13 11:47:55 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,9 +1,6 @@
 import requests
 
-def test_login():
-    login_url = 'https://profile.w3schools.com/log-in?redirect_url=https%3A%2F%2Fmy-learning.w3schools.com%2F'
-    data = {'username': 'rohithr9701@gmail.com', 'password': 'rohith@120'}
-    response = requests.post(login_url, data=data)
+
     
     assert response.status_code == 200
     assert 'Welcome' in response.text
