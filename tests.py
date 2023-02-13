6ac25856c28049d940fac036718835267adefa05
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-02-13 10:38:53 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 # Set up Chrome driver
 driver = webdriver.Chrome()
@@ -10,7 +9,7 @@ driver.get("https://www.google.com")
 # Find search bar and enter text
 search_bar = driver.find_element_by_name("q")
 search_bar.send_keys("Selenium")
-search_bar.send_keys(Keys.RETURN)
+search_bar.submit()
 
 # Assert that "Selenium" is in the title of the results page
 assert "Selenium" in driver.title
