44f47efd2e9553cd10e19a3a321b9e70ca7a5ad2
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-02-13 11:45:54 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,8 +1,16 @@
 import requests
 
-def test_web_page():
-    url = 'https://www.example.com/'
-    response = requests.get(url)
+def test_login():
+    login_url = 'https://profile.w3schools.com/log-in?redirect_url=https%3A%2F%2Fmy-learning.w3schools.com%2F'
+    data = {'username': 'rohithr9701@gmail.com', 'password': 'rohith@123'}
+    response = requests.post(login_url, data=data)
     
     assert response.status_code == 200
-    assert 'Example Domain' in response.text
+    assert 'Welcome' in response.text
+
+def test_profile():
+    profile_url = 'https://profile.w3schools.com/log-in?redirect_url=https%3A%2F%2Fmy-learning.w3schools.com%2F'
+    response = requests.get(profile_url)
+    
+    assert response.status_code == 200
+    assert 'My Profile' in response.text
