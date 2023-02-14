47f229a712f6e2f1438bc28c7c25088eeac220d2
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-02-14 10:39:24 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,20 +1,10 @@
-from Selenium import webdriver  
-import time  
-from Selenium.webdriver.common.keys import Keys  
-print("sample test case started")  
-driver = webdriver.Chrome()  
-#driver=webdriver.firefox()  
-#driver=webdriver.ie()  
-#maximize the window size  
-driver.maximize_window()  
-#navigate to the url  
-driver.get("https://www.google.com/")  
-#identify the Google search text box and enter the value  
-driver.find_element_by_name("q").send_keys("javatpoint")  
-time.sleep(3)  
-#click on the Google search button  
-driver.find_element_by_name("btnK").send_keys(Keys.ENTER)  
-time.sleep(3)  
-#close the browser  
-driver.close()  
-print("sample test case successfully completed")  
+from selenium import webdriver
+
+# Create a Chrome driver instance
+driver = webdriver.Chrome()
+
+# Use the driver to automate browser actions
+# (e.g. navigating to a URL, clicking on elements, etc.)
+
+# Close the driver instance to release the browser resources
+driver.quit()
