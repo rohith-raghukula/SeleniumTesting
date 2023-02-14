684ca8ccd207358217a8a7bb0eccf229a3828371
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-02-14 11:46:52 +0530

    Update one_test.py

--- test/one_test.py
+++ test/one_test.py
@@ -1,25 +1,21 @@
 from selenium import webdriver
 
-class TestGoogleSearch():
-    @classmethod
-    def setup_class(cls):
-        # Create a new Chrome browser instance
-        cls.driver = webdriver.Chrome()
-        cls.driver.implicitly_wait(10)
 
-    @classmethod
-    def teardown_class(cls):
-        # Close the browser
-        cls.driver.quit()
 
-    def test_google_search(self):
-        # Navigate to the Google homepage
-        self.driver.get("https://www.google.com")
+def test_google_search():
+    # Create a Chrome driver instance
+    driver = webdriver.Chrome()
 
-        # Find the search input element and enter a search query
-        search_box = self.driver.find_element_by_name("q")
-        search_box.send_keys("python selenium")
-        search_box.submit()
+    # Navigate to Google search page
+    driver.get("https://www.google.com/")
 
-        # Assert that the search results page was loaded successfully
-        assert "Python Selenium - Google Search" in self.driver.title
+    # Locate the search bar element and enter a search query
+    search_box = driver.find_element_by_name("q")
+    search_box.send_keys("python selenium")
+    search_box.submit()
+
+    # Verify that the search results page was loaded successfully
+    assert "Python Selenium - Google Search" in driver.title
+
+    # Close the driver instance
+    driver.quit()
