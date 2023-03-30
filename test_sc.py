ff6b19eb09014edaaa31c17a2231e87f8f10fe5c
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-03-30 19:47:47 +0530

    Update test_sc.py

--- test_sc.py
+++ test_sc.py
@@ -1,11 +1,11 @@
 import unittest
-import self
+
 from selenium import webdriver
-from selenium.webdriver import ActionChains
+
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+
 from selenium.webdriver.common.action_chains import ActionChains
 
 
