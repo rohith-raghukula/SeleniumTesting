d8c5dc986e68bd29ec7b4f2de72da8028e6169ea
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-08-04 13:46:23 +0530

    Update test_test.py

--- test_test.py
+++ test_test.py
@@ -1,16 +1,10 @@
 import unittest
-import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 import time
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-import tracemalloc
+
 
 
 class TestStezyLogin(unittest.TestCase):
@@ -29,3 +23,37 @@ class TestStezyLogin(unittest.TestCase):
     def tearDownClass(cls):
         # Close the browser
         cls.driver.quit()
+
+    def test_stezy_login(self):
+        # Navigate to the app.stezy.io homepage
+        self.driver.get("https://app.stezy.io")
+
+        # Find the email and password input fields and enter login credentials
+        email_input = self.driver.find_element(By.NAME, "username")
+        email_input.send_keys("rohit@stezy.io")
+
+        password_input = self.driver.find_element(By.NAME, "Password")
+        password_input.send_keys("Stezy@123")
+
+        # Find the login button and click it
+        login_button = self.driver.find_element(By.NAME, "LogIn")
+        login_button.click()
+
+        time.sleep(10)
+
+
+# Check if login was successful
+        self.assertIn("dashboard", self.driver.current_url, "Login failed")
+        # Click on the Vulnerability Check link
+        EFS_link = self.driver.find_element(By.LINK_TEXT, "EFS")
+        EFS_link.click()
+
+        # Find the "Files" tab and click it
+        files_tab = self.driver.find_element(By.XPATH, "//div[text()='Files']")
+        files_tab.click()
+        time.sleep(5)
+        download_button = self.driver.find_element(By.XPATH,
+                                                  "//span[contains(@class, 'pointer') and contains(text(), 'Download')]")
+        download_button.click()
+
+        time.sleep(5)
