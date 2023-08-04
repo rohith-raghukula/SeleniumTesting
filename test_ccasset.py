1fc9c5d6dd4a6b5451a6fc42b6bc65a1b71cc799
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-08-04 12:58:19 +0530

    Update test_ccasset.py

--- test_ccasset.py
+++ test_ccasset.py
@@ -1,27 +1,29 @@
 import unittest
-import os
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+
+
 import time
+
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-import tracemalloc
+
 
 class TestStezyLogin(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        firefox_options = FirefoxOptions()
-        firefox_options.headless = True
-        geckodriver_path = os.path.join(os.path.dirname(__file__), 'geckodriver')
-        service = Service('path/to/geckodriver')
-        cls.driver = webdriver.Firefox(options=firefox_options, service=service)
+        # Set up Chrome options to run in headless mode
+        options = Options()
+        #options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+        # Create a new Chrome browser instance with the headless options
+        cls.driver = webdriver.Chrome(options=options)
         cls.driver.implicitly_wait(10)
+
     @classmethod
     def tearDownClass(cls):
         # Close the browser
@@ -45,47 +47,81 @@ class TestStezyLogin(unittest.TestCase):
         login_button.click()
 
 
-
+        print("hi")
         time.sleep(10)
         # Check if login was successful
         self.assertIn("dashboard", self.driver.current_url, "Login failed")
 
+        # Find the Blockchain element and click it
+        blockchain_button = self.driver.find_element(By.XPATH, "//span[@class='nav-label' and text()='Blockchain']")
+        blockchain_button.click()
         time.sleep(5)
-        chaincode_link = self.driver.find_element(By.XPATH, '//span[text()="Chaincode"]')
-        chaincode_link.click()
+
+        create_private_element = self.driver.find_element(By.XPATH, '//span[@class="nav-label" and text()="Private"]')
+        create_private_element.click()
         time.sleep(5)
-        CC_asset_link = self.driver.find_element(By.XPATH, '//p[text()="Design Chaincode Asset"]')
-        CC_asset_link.click()
+
+        consotrium_name_input = self.driver.find_element(By.NAME, "name")
+        consotrium_name_input.send_keys("eth rohith test")
         time.sleep(5)
-        variable_input = self.driver.find_element(By.NAME, "form-name")
-        variable_input.send_keys("cctest")
+        description_textarea = self.driver.find_element(By.TAG_NAME, "textarea")
+        description_textarea.send_keys("hi")
         time.sleep(5)
-        add_button = self.driver.find_element(By.XPATH, "//button[text()='Add']")
-        add_button.click()
         time.sleep(5)
-        variable_input = self.driver.find_element(By.NAME, "form-name")
-        variable_input.send_keys("test1")
-        time.sleep(2)
-        # Find the dropdown menu and select the "Integer" option
-        integer_option = self.driver.find_element(By.XPATH, "//option[@value='integer']")
-        integer_option.click()
+        next_button = self.driver.find_element(By.XPATH, "//button[@class='ml-auto' and text()='Next']")
+        next_button.click()
         time.sleep(5)
-        # Add text input field
-        text_input_field = self.driver.find_element(By.NAME, "smart-contract-name")
-        text_input_field.send_keys("test-sc")
+        # select blockchain and blockchain type
+        ethereum_button = self.driver.find_element(By.XPATH,
+                                                   "//div[@class='card ng-star-inserted']//img[contains(@src, 'ethereum.png')]")
+        ethereum_button.click()
         time.sleep(5)
-        add_button = self.driver.find_element(By.XPATH, "//button[text()='Add']")
-        add_button.click()
+        poa_button = self.driver.find_element(By.XPATH,
+                                              "//img[@class='ng-star-inserted' and contains(@src, 'poa.jpeg')]")
+        poa_button.click()
         time.sleep(5)
-        wait = WebDriverWait(driver, 10)
-        deploy_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Deploy')]")))
-        actions = ActionChains(driver)
-        actions.move_to_element(deploy_button).click().perform()
-        # Click the deploy button
-        deploy_button.click()
+        next_button = self.driver.find_element(By.XPATH, "//button[text()='Next']")
+        driver.execute_script("arguments[0].click();", next_button)
+        time.sleep(5)
+        SZM_element = self.driver.find_element(By.XPATH, '//img[@src="https://cdn.stezy.io/assets/images/cloud/stezy.jpeg"]')
+        SZM_element.click()
+        time.sleep(5)
+        next_button = self.driver.find_element(By.XPATH, "//button[text()='Next']")
+        driver.execute_script("arguments[0].click();", next_button)
+        name_element = self.driver.find_element(By.NAME, "name")
+        name_element.send_keys("node1")
+        description_element = self.driver.find_element(By.XPATH,'//textarea[@placeholder="Example: Node description"]')
+        description_element.send_keys("node des")
+
+        dropdown_element = self.driver.find_element(By.NAME,'region')
+        dropdown_element.click()
+
+
+        # Wait for the dropdown options to be visible
+        wait = WebDriverWait(self.driver, 10)
+        dropdown_options = wait.until(
+            EC.visibility_of_all_elements_located((By.XPATH, "//select[@name='region']/option")))
+
+        # Loop through the dropdown options and select the desired option by its text
+        for option in dropdown_options:
+            if option.text == 'East Asia':
+                option.click()
+                break
+
+        time.sleep(10)
+        wait = WebDriverWait(self.driver, 10)
+        instance_options = wait.until(
+            EC.visibility_of_all_elements_located((By.XPATH, "//select[@name='type']/option")))
+        # Click on the option element
+        # Loop through the dropdown options and select the desired option by its text
+        for option in instance_options:
+            if option.text == 'Standard_A2_v2 (2 VCPUs, 4 GB)':
+                option.click()
+                break
 
         time.sleep(5)
 
-if __name__ == '__main__':
-    tracemalloc.start()
-    unittest.main()
+        save_button = driver.find_element(By.CLASS_NAME, "ml-auto")
+        save_button.click()
+        time.sleep(5)
+
