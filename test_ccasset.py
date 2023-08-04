6c8e3d12d3908c166c4f1674f0959eb845df1bdd
rohith-raghukula <88700461+rohith-raghukula@users.noreply.github.com>
2023-08-04 12:26:19 +0530

    Update test_ccasset.py

--- test_ccasset.py
+++ test_ccasset.py
@@ -6,71 +6,81 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 class TestStezyLogin(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        # Set up Firefox options to run in headless mode
-        options = FirefoxOptions()
-
-        options.headless = True
-        service = Service('path/to/geckodriver')
-        # Create a new Firefox browser instance with the headless options
-        cls.driver = webdriver.Firefox(options=options, service=service)
+        # Set up Chrome options to run in headless mode
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+        # Create a new Chrome browser instance with the headless options
+        cls.driver = webdriver.Chrome(options=options)
         cls.driver.implicitly_wait(10)
 
     @classmethod
     def tearDownClass(cls):
         # Close the browser
         cls.driver.quit()
-
     def test_stezy_login(self):
 
         # Navigate to the app.stezy.io homepage
         driver = self.driver
         driver.get("https://app.stezy.io")
+
         # Find the email and password input fields and enter login credentials
         email_input = driver.find_element(By.NAME, "username")
         email_input.send_keys("rohit@stezy.io")
+
         password_input = driver.find_element(By.NAME, "Password")
         password_input.send_keys("Stezy@123")
-      # Find the login button and click it
+
+        # Find the login button and click it
         login_button = driver.find_element(By.NAME, "LogIn")
         login_button.click()
+
+
+
         time.sleep(10)
         # Check if login was successful
         self.assertIn("dashboard", self.driver.current_url, "Login failed")
+
         time.sleep(5)
         chaincode_link = self.driver.find_element(By.XPATH, '//span[text()="Chaincode"]')
         chaincode_link.click()
         time.sleep(5)
-        SC_asset_link = self.driver.find_element(By.XPATH, '//p[text()="Chaincode From Scratch"]')
-        SC_asset_link.click()
+        CC_asset_link = self.driver.find_element(By.XPATH, '//p[text()="Design Chaincode Asset"]')
+        CC_asset_link.click()
         time.sleep(5)
-        formtitle_input = self.driver.find_element(By.NAME, "formtitle")
-        formtitle_input.send_keys("test1")
+        variable_input = self.driver.find_element(By.NAME, "form-name")
+        variable_input.send_keys("cctest")
+        time.sleep(5)
+        add_button = self.driver.find_element(By.XPATH, "//button[text()='Add']")
+        add_button.click()
+        time.sleep(5)
+        variable_input = self.driver.find_element(By.NAME, "form-name")
+        variable_input.send_keys("test1")
+        time.sleep(2)
+        # Find the dropdown menu and select the "Integer" option
+        integer_option = self.driver.find_element(By.XPATH, "//option[@value='integer']")
+        integer_option.click()
         time.sleep(5)
         # Add text input field
-        text_input_field = self.driver.find_element(By.XPATH, "//li[@data-type='text']")
-        text_input_field.click()
+        text_input_field = self.driver.find_element(By.NAME, "smart-contract-name")
+        text_input_field.send_keys("test-sc")
         time.sleep(5)
-
-        time.sleep(2)
-        # Add number input field
-        number_input_field = self.driver.find_element(By.XPATH, "//li[@data-type='number']")
-        number_input_field.click()
-
-        time.sleep(10)
+        add_button = self.driver.find_element(By.XPATH, "//button[text()='Add']")
+        add_button.click()
+        time.sleep(5)
         wait = WebDriverWait(driver, 10)
         deploy_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Deploy')]")))
         actions = ActionChains(driver)
         actions.move_to_element(deploy_button).click().perform()
-         #Click the deploy button
+        # Click the deploy button
         deploy_button.click()
+
         time.sleep(5)
 
-    if __name__ == '__main__':
-        unittest.main()
+if __name__ == '__main__':
+    unittest.main()
